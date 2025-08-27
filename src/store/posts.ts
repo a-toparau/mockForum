c37d0b19806370d8e657b7037331a7558944db32
Aleksei <a.toparau@andersenlab.com>
2025-08-27 16:34:18 +0300

    feature: upload posts store

---
+++ src/store/posts.ts
@@ -0,0 +1,104 @@
+import { create } from 'zustand';
+import { fetchPosts, fetchCommentsByPostId } from '@/api/posts';
+import type { IComment, IPost } from '@/types/post';
+import { devtools, persist } from 'zustand/middleware';
+import { useNotificationStore } from './notification';
+
+interface IPostsState {
+  posts: IPost[];
+  comments: Record<number, IComment[]>;
+  favoritePosts: number[];
+  reactions: Record<number, boolean>;
+  loading: boolean;
+  error: string | null;
+
+  getAllPosts: () => Promise<void>;
+  getPostWithComments: (id: number) => Promise<{ post?: IPost; postComments: IComment[] }>;
+  react: (id: number, value: boolean) => void;
+  toggleFavoritePost: (id: number) => void;
+}
+
+export const usePostsStore = create<IPostsState>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        posts: [],
+        comments: {},
+        favoritePosts: [],
+        reactions: {},
+        loading: false,
+        error: null,
+
+        getAllPosts: async () => {
+          const notifications = useNotificationStore.getState();
+          set({ loading: true, error: null });
+
+          try {
+            const data = await fetchPosts();
+            set({ posts: data });
+          } catch {
+            notifications.showNotification('error', 'Не удалось загрузить посты');
+          } finally {
+            set({ loading: false });
+          }
+        },
+
+        getPostWithComments: async (id) => {
+          const notifications = useNotificationStore.getState();
+          try {
+            set({ loading: true, error: null });
+
+            const post = get().posts.find(({ id: postId }) => postId === id);
+
+            let comments = get().comments[id];
+            if (!comments) {
+              const remoteComments = await fetchCommentsByPostId(id);
+              set({ comments: { ...get().comments, [id]: remoteComments } });
+              comments = remoteComments;
+            }
+
+            return { post, postComments: comments ?? [] };
+          } catch {
+            notifications.showNotification('error', 'Не удалось загрузить посты');
+
+            return { post: undefined, postComments: [] };
+          } finally {
+            set({ loading: false });
+          }
+        },
+
+        react: (id: number, value: boolean | undefined) => {
+          set(
+            (state) => ({
+              posts: state.posts.map((post) =>
+                post.id === id ? { ...post, like: post.like === value ? undefined : value } : post,
+              ),
+            }),
+            false,
+            `react(${id}, ${value})`,
+          );
+        },
+
+        toggleFavoritePost: (id: number) => {
+          const favs = get().favoritePosts;
+          const postIndex = favs.indexOf(id);
+
+          postIndex !== -1 ? favs.splice(postIndex, 1) : favs.push(id);
+
+          set({ favoritePosts: favs }, false, `toggleFavorite(${id})`);
+        },
+      }),
+
+      { name: 'postStorage' },
+    ),
+    {
+      name: 'postStorage',
+      partialize: ({ posts, comments, favoritePosts, reactions }: IPostsState) => ({
+        posts,
+        comments,
+        favoritePosts,
+        reactions,
+      }),
+    },
+  ),
+);
